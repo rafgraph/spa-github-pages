1b48d263311db7bfb5db0d2c219625d86edd7e38
rafrex <code@rafrex.com>
2016-05-03 10:28:05 -0700

    Add react-router and App component

---
+++ components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App({ children }) {
+
+  return (
+    <div>
+      <div>Hello world</div>
+      {children}
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/App';
+
+const routes = (
+  <Route path='/' component={App} />
+);
 
 render(
-  <div>Hello world</div>,
+  <Router history={browserHistory}>{routes}</Router>,
   document.getElementById('root')
 )
