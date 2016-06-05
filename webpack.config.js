8d03ad21cff588261f25a538ff904962bc6f946d
rafrex <code@rafrex.com>
2016-06-04 22:59:16 -0700

    Remove production node environment plugin from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
-const webpack = require('webpack');
-
 module.exports = {
   context: __dirname,
   entry: './index.js',
   output: {
-    path: __dirname + '/__build__',
-    filename: 'bundle.js'
+    path: `${__dirname}/__build__`,
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
@@ -14,19 +12,12 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ]
 };
