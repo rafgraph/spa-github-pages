ceaa32841eb8892a04e52ed03c47792b4857f2fb
rafrex <code@rafrex.com>
2016-06-05 19:57:03 -0700

    Create babelrc config file

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,6 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel',
-        query: {
-          presets: ['es2015', 'react'],
-        },
       },
     ],
   },
