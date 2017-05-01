434ff1d8f9c9f3ed24cf939a9b305c37cfa3be84
rafrex <code@rafrex.com>
2017-05-01 13:28:19 -0700

    Update react

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Interactive from 'react-interactive';
 import { Link } from 'react-router';
 import s from '../styles/exampleComponent.style';
