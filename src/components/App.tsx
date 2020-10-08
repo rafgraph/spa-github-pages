68ba5b63f505980c6cf14e47f10b38339b6a2fee
Rafael Pedicini <rafael@rafgraph.dev>
2020-10-08 11:04:38 -0400

    Update centering style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,29 +14,13 @@ const rootStyle: CSS.Properties = {
   fontWeight: 300,
   fontSize: '16px',
   letterSpacing: '0.025em',
-  padding: '3vh 0 12vh 0',
-  width: '500px',
-  // use responsive max-width to simulate padding/margin to allow
-  // space for vertical scroll bar without creating horizontal scroll bar
-  // (if there is padding, the window will scroll horizontally to show the padding)
-  maxWidth: 'calc(100vw - 40px)',
   boxSizing: 'border-box',
-
-  // center based on vw to prevent content jump when vertical scroll bar show/hide
-  // note: vw/vh include the width of scroll bars. Note that centering using margin auto
-  // or % (which doesn't include scroll bars, so changes when scroll bars shown) causes a page jump
-  position: 'relative',
-  left: '50vw',
-  WebkitTransform: 'translate(-50%, 0)',
-  // MozTransform: 'translate(-50%, 0)',
-  msTransform: 'translate(-50%, 0)',
-  OTransform: 'translate(-50%, 0)',
-  transform: 'translate(-50%, 0)',
-
   WebkitTextSizeAdjust: 'none',
-  MozTextSizeAdjust: 'none',
-  // msTextSizeAdjust: 'none',
   textSizeAdjust: 'none',
+
+  maxWidth: '540px',
+  margin: '0 auto',
+  padding: '3vh 20px 12vh 20px',
 };
 
 const titleStyle: CSS.Properties = {
